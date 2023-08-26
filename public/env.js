ed0b114b3db211ddba75774d98b1d79ec534bb91
이승규 <unhappydogchew@gmail.com>
2023-08-27 01:05:12 +0900

    chore: react-dotenv 설정 및 api_key 환경변수에 추가

---
+++ public/env.js
@@ -0,0 +1,4 @@
+window.env = {
+  "API_URL": "https://api.themoviedb.org/3",
+  "API_TOKEN": "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0Njc2YTI0YzVkNGUwZDJjMGFjZjEzYjVhMWQ0NGM1YyIsInN1YiI6IjY0ZTg5MGYzOTBlYTRiMDBjNzM5NmU2YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Rl0CcJmSVYBYQULNSl606D5yM2Mlcz546lpleVVHlhY"
+};
