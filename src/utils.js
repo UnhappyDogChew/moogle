f9a3fb0d592b5d32a2b52a20bb0df69ee6f9703c
이승규 <unhappydogchew@gmail.com>
2023-08-28 06:17:19 +0900

    fix: 특수문자가 들어간 타이틀 선택 안됨 문제 해결

--- src/utils.js
+++ src/utils.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const parseSearch = (search) => {
   const result = {};
-  const regex = /([a-zA-Z0-9\_]+)=([a-zA-Z0-9\_\%]+)/g;
+  const regex = /([a-zA-Z0-9\_]+)=([a-zA-Z0-9\_\%\:\!\-]+)/g;
   let arr = regex.exec(search);
   while (arr) {
     result[arr[1]] = decodeURIComponent(arr[2]);
