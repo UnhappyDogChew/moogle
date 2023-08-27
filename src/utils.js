f55916a6232539c3e68d80835a2dd6a5e5f37f4e
이승규 <unhappydogchew@gmail.com>
2023-08-27 23:25:29 +0900

    fix: parseSearch 함수 수정

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,10 @@
 export const parseSearch = (search) => {
   const result = {};
-  const regex = /([a-zA-Z0-9\_])=([a-zA-Z0-9\_])/g;
+  const regex = /([a-zA-Z0-9\_]+)=([a-zA-Z0-9\_\%]+)/g;
   let arr = regex.exec(search);
   while (arr) {
-    result[arr[1]] = arr[2];
+    result[arr[1]] = decodeURIComponent(arr[2]);
+    arr = regex.exec(search);
   }
   return result;
 };
